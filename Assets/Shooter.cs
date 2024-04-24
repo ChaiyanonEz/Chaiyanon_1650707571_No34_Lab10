3a9b7e243b0b7369c21d7da7022a84f77bf0f3c7
ChaiyanonEz <chaiyanon.prom@bumail.net>
2024-04-24 13:51:55 +0700

    Add Script

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public Camera Camera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,25 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.magenta, 10f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
+
+            if(hit.collider != null )
+            {
+                target.transfrom.position = new Vector3(hit.point.x, hit.point.y);
+                Debug.Log($"hit point : ({hit.point.x}, {hit.point.y}");
+
+                Vector2 projectile = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                Rigidbody firedBullet = Instantiate(bulletPrefab,shootPoint.position,Quaternion.identity);
+                firedBullet.velocity = projectile;
+
+            }
+
+        }
         
     }
 }
