d4c10ff0135af770cadffa81f8de4e011862d3a3
ChaiyanonEz <chaiyanon.prom@bumail.net>
2024-04-24 14:41:25 +0700

    Update Script

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -8,7 +8,9 @@ public class Projectile : MonoBehaviour
     [SerializeField] Transform shootPoint;
     [SerializeField] GameObject target;
     [SerializeField] Rigidbody2D bulletPrefab;
-    void start()
+
+    // Start is called before the first frame update
+    void Start()
     {
 
     }
@@ -16,34 +18,34 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(ray.origin, ray.direction * 10f, Color.magenta, 10f);
 
-            RaycastHit2D hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
 
-            if(hit.collider != null )
+            if (hit.collider != null)
             {
-                target.transfrom.position = new Vector3(hit.point.x, hit.point.y);
-                Debug.Log($"hit point : ({hit.point.x}, {hit.point.y}");
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log($"hit point : {hit.point.x}, {hit.point.y}");
 
                 Vector2 projectile = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
-                Rigidbody firedBullet = Instantiate(bulletPrefab,shootPoint.position,Quaternion.identity);
-                firedBullet.velocity = projectile;
+
+                Rigidbody2D firebullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+                firebullet.velocity = projectile;
             }
+
         }
+    }
 
-        Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float t);
-        {
-            Vector2 distance = taget - origin;
-            float velocityX = distance.x / t ;
-            float velocityY = distance.y / t + 0.5f * Mathf.Abs(Physics2D.gravity.y);
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float t)
+    {
+        Vector2 distance = target - origin;
+        float velocityX = distance.x / t;
+        float velocityY = distance.y / t + 0.5f * Mathf.Abs(Physics2D.gravity.y);
 
-            Vector2 result = new Vector2 (velocityX, velocityY);
-            return result;
-        }
-
+        Vector2 result = new Vector2(velocityX, velocityY);
+        return result;
     }
 }
